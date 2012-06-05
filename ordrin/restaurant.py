1ffd481a2f8458b72b9692ea0a2fe5d562db2aca
Michael Lumish <michael.lumish@gmail.com>
2012-06-05 15:31:16 -0400

    Fixed docstrings to conform to Python standards

--- ordrin/restaurant.py
+++ ordrin/restaurant.py
@@ -4,20 +4,20 @@ class RestaurantAPI(OrdrinAPI):
   """This class will be used to access the restaurant API"""
 
   def get_delivery_list(date_time, address):
-    """Calls the dl API function with the given arguments."""
+    """Call the dl API function with the given arguments."""
     dt = self._get_asap_or_datetime(date_time)
     return self._call_api('GET', ('dl', dt, address.zip, address.city, address.addr))
 
   def get_delivery_check(restaurant_id, date_time, address):
-    """Calls the dc API function with the given arguments."""
+    """Call the dc API function with the given arguments."""
     dt = self._get_asap_or_datetime(date_time)
     return self._call_api('GET', ('dc', restaurant_id, dt, address.zip, address.city, address.addr))
 
   def get_fee(restaurant_id, subtotal, tip, date_time, address):
-    """Calls the fee API function with the given arguments."""
+    """Call the fee API function with the given arguments."""
     dt = self._get_asap_or_datetime(date_time)
     return self._call_api('GET', ('fee', subtotal, tip, dt, address.zip, address.city, address.addr))
 
   def get_details(restaurant_id):
-    """Calls the rd API function with the given id."""
+    """Call the rd API function with the given id."""
     return self._call_api('GET', ('rd', restaurant_id))
