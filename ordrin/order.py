c67eaef8b18a8a547de81bd2df60621bf4f15c58
Michael Lumish <michael.lumish@gmail.com>
2012-06-05 11:48:09 -0400

    Added function for ordering food and creating a user

--- ordrin/order.py
+++ ordrin/order.py
@@ -17,4 +17,19 @@ class Order(OrdrinAPI):
     data['em'] = email
     data.update(credit_card.make_dict())
     return _call_api('POST', ('o', restaurant_id), login=login, data=data)
+
+  def order_create_user(restaurant_id, tray, tip, delivery_date_time, first_name, last_name, address, email, password, credit_card):
+    data = {'restaurant_id':restaurant_id, 'tray':str(tray), 'tip':tip}
+    data['delivery_date'] = _get_asap_or_date(delivery_date_time)
+    if data['delivery_date'] != 'ASAP':
+      data['delivery_time'] = delivery_date_time.strftime('%H:%M')
+    data['first_name'] = first_name
+    data['last_name'] = last_name
+    data.update(address.make_dict())
+    login = Login(email, password)
+    data['em'] = email
+    data['password'] = login.password
+    data.update(credit_card.make_dict())
+    return _call_api('POST', ('o', restaurant_id), login=login, data=data)
+    
     
