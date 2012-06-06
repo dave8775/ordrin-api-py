acc09ae45198f93122904e10401971957140f2be
Michael Lumish <michael.lumish@gmail.com>
2012-06-06 12:10:47 -0400

    Normalized data going into restaurant API calls

--- ordrin/restaurant.py
+++ ordrin/restaurant.py
@@ -1,5 +1,5 @@
 from ordrinapi import OrdrinAPI
-from ordrindata import _validate
+from normalize import normalize
 
 class RestaurantAPI(OrdrinAPI):
   """This class will be used to access the restaurant API"""
@@ -12,17 +12,18 @@ class RestaurantAPI(OrdrinAPI):
   def get_delivery_check(self, restaurant_id, date_time, address):
     """Call the dc API function with the given arguments."""
     dt = self._get_asap_or_datetime(date_time)
-    _validate(restaurant_id, r'^\d+$', "restaurant_id must be a natural number")
+    restaurant_id = normalize(restaurant_id, 'number')
     return self._call_api('GET', ('dc', restaurant_id, dt, address.zip, address.city, address.addr))
 
   def get_fee(self, restaurant_id, subtotal, tip, date_time, address):
     """Call the fee API function with the given arguments."""
     dt = self._get_asap_or_datetime(date_time)
-    _validate(restaurant_id, r'^\d+$', "restaurant_id must be a natural number")
-    _validate(subtotal, r'^\d+(\.\d+)?', "Subtotal must be money")
+    restaurant_id = normalize(restaurant_id, 'number')
+    subtotal = normalize(subtotal, 'money')
+    tip = normalize(tip, 'money')
     return self._call_api('GET', ('fee', restaurant_id, subtotal, tip, dt, address.zip, address.city, address.addr))
 
   def get_details(self, restaurant_id):
     """Call the rd API function with the given id."""
-    _validate(restaurant_id, r'^\d+$', "restaurant_id must be a natural number")
+    restaurant_id = normalize(restaurant_id, 'number')
     return self._call_api('GET', ('rd', restaurant_id))
