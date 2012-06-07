a7b618e71fda9f82af319fc167d09692e1cfedba
Michael Lumish <michael.lumish@gmail.com>
2012-06-07 16:35:50 -0400

    Added order demo functions

--- demo/demo.py
+++ demo/demo.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+import functools
 from pprint import pprint
 import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,11 +8,13 @@ os.sys.path.insert(0,parentdir)
 
 import ordrin
 import ordrin.data
+import ordrin.errors
 
 #
-# Helper Decorator
+# Helper Decorators
 #
 def print_docstring_header(f):
+  @functools.wraps(f)
   def g(*args, **kwargs):
     print ''
     print f.__doc__
@@ -19,17 +22,36 @@ def print_docstring_header(f):
     return f(*args, **kwargs)
   return g
 
+def print_api_errors(f):
+  @functools.wraps(f)
+  def g(*args, **kwarsg):
+    try:
+      return f(*args, **kwargs)
+    except ordrin.errors.ApiError as e:
+      print "The API returned the following error:"
+      print e
+
 #
 # Global Variables
 #
 test_urls = {'restaurant_url': 'https://r-test.ordr.in/',
-             'user_url': 'https://u-test.ordr.in/',
-             'order_url': 'https://o-test.ordr.in/'}
+       'user_url': 'https://u-test.ordr.in/',
+       'order_url': 'https://o-test.ordr.in/'}
 api_key = raw_input("Please input your API key: ")
 
-api = ordrin.APIs(api_key, **test_urls) #Create an API object
+api = ordrin.APIs(api_key, **test_urls) # Create an API object
+
+# Create an Address object
 address = ordrin.data.Address('1 Main Street', 'College Station', 'TX', '77840', '(555) 555-5555')
-credit_card = ordrin.data.CreditCard('Test User', '01', str(datetime.date.today().year+2), address, '4111111111111111', '123')
+address_nick = 'addr1'
+
+# Create a CreditCard object
+first_name = 'Test'
+last_name = 'User'
+credit_card = ordrin.data.CreditCard(first_name+' '+last_name, '01', str(datetime.date.today().year+2), address, '4111111111111111', '123')
+credit_card_nick = 'cc1'
+
+#Create a UserLogin object
 email = 'demo+{}@ordr.in'.format(uuid.uuid1().hex)
 password = 'password'
 login = ordrin.data.UserLogin(email, password)
@@ -70,23 +92,47 @@ def detail_demo(restaurant_id):
   """Get detailed information about a single restaurant"""
   restaurant_detail = api.restaurant.get_details(restaurant_id)
   pprint(restaurant_detail, indent=4, depth=3)
+  return restauran_detail
 
 #
 # Order demo functions
 #
 @print_docstring_header
+@print_api_errors
 def anonymous_order_demo(restaurant_id, tray):
   """Order food as someone without a user account"""
   tip = '$3.00'
-  response = api.order.order(restaurant_id, tray, tip, 'ASAP', 'Test', 'User', address, credit_card, email)
+  response = api.order.order(restaurant_id, tray, tip, 'ASAP', first_name, last_name, address, credit_card, email=email)
   pprint(response)
 
+@print_docstring_header
+@print_api_errors
+def create_user_and_order_demo(restaurant_id, tray):
+  """Order food and create an account"""
+  tip = '$3.00'
+  response = api.order.order(restaurant_id, tray, tip, 'ASAP', first_name, last_name, address, credit_card, email, password)
+  pprint(response)
+
+@print_docstring_header
+@print_api_errors
+def order_with_nicks_demo(restaurant_id, tray):
+  """Order food as a logged in user using previously stored address and credit card"""
+  tip = '$3.00'
+  response = api.order.order(restaurant_id, tray, tip, 'ASAP', first_name, last_name, address_nick, credit_card_nick, login=login)
+  pprint(response)
+  
 #
 # Main
 #
-delivery_list = delivery_list_immediate_demo()
-delivery_list_future_demo()
-restaurant_id = delivery_list[0]['id']
-delivery_check_demo(restaurant_id)
-fee_demo(restaurant_id)
-detail_demo(restaurant_id)
+run_demo():
+  """Run through the entire demo sequence"""
+  delivery_list = delivery_list_immediate_demo()
+  delivery_list_future_demo()
+  restaurant_id = delivery_list[0]['id']
+  delivery_check_demo(restaurant_id)
+  fee_demo(restaurant_id)
+  detail = detail_demo(restaurant_id)
+
+if __name__=='__main__':
+  run_demo()
+  
