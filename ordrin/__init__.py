fa5b73c5fb508bf6fb76de9608a0e11cf854b68f
Michael Lumish <michael.lumish@gmail.com>
2012-06-08 13:59:03 -0400

    Expanded package documentation

--- ordrin/__init__.py
+++ ordrin/__init__.py
@@ -5,8 +5,19 @@ class APIs(object):
 
   def __init__(self, api_key, restaurant_url=None, user_url=None, order_url=None):
     """Sets up this module to make API calls. The first argument is the developer's
-    API key. The other three are the urls corresponding to the three parts of the api.
-    No API calls will work until this function is called"""
+    API key. The other three are the URLs corresponding to the three parts of the api.
+    No API calls will work until this function is called. API objects will only be
+    instantiated for URLs that are passed in.
+
+    Arguments:
+    api_key -- The developer's API key
+
+    Keyword Arguments:
+    restaurant_url -- The base url for the restaurant API
+    user_url -- The base url for the user API
+    order_url -- The base url for the order API
+
+    """
     if restaurant_url:
       self.restaurant = restaurant.RestaurantApi(api_key, restaurant_url)
     if user_url:
