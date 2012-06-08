a7f8b24a9b1ed02ad81641e90fc998ca0c3a51d1
Michael Lumish <michael.lumish@gmail.com>
2012-06-08 12:15:36 -0400

    Expanded restaurant API documentation

--- ordrin/restaurant.py
+++ ordrin/restaurant.py
@@ -2,21 +2,47 @@ from ordrinapi import OrdrinApi
 from normalize import normalize
 
 class RestaurantApi(OrdrinApi):
-  """This class will be used to access the restaurant API"""
+  """This object's methods access the ordr.in restaurant API"""
 
   def get_delivery_list(self, date_time, address):
-    """Call the dl API function with the given arguments."""
+    """Get a list of dicts representing restaurants that will deliver to the
+    given address at the given time. 
+
+    Arguments:
+    date_time -- Either 'ASAP' or a datetime object in the future
+    address -- the address to deliver to. Should be an ordrin.data.Address object
+
+    """
     dt = normalize(date_time, 'datetime')
     return self._call_api('GET', ('dl', dt, address.zip, address.city, address.addr))
 
   def get_delivery_check(self, restaurant_id, date_time, address):
-    """Call the dc API function with the given arguments."""
+    """Get data about a given restaurant, including whether it will deliver to
+    the specified address at the specified time
+
+    Arguments:
+    restaurant_id -- Ordr.in's restaurant identifier
+    date_time -- Either 'ASAP' or a datetime object in the future
+    address -- the address to deliver to. Should be an ordrin.data.Address object
+
+    """
     dt = normalize(date_time, 'datetime')
     restaurant_id = normalize(restaurant_id, 'number')
     return self._call_api('GET', ('dc', restaurant_id, dt, address.zip, address.city, address.addr))
 
   def get_fee(self, restaurant_id, subtotal, tip, date_time, address):
-    """Call the fee API function with the given arguments."""
+    """Get data about a given restaurant, including whether it will deliver to
+    the specified address at the specified time, and what the fee will be on an
+    order with the given subtotal and tip
+
+    Arguments:
+    restaurant_id -- Ordr.in's restaurant identifier
+    subtotal -- the subtotal of the order
+    tip -- the tip on the order
+    date_time -- Either 'ASAP' or a datetime object in the future
+    address -- the address to deliver to. Should be an ordrin.data.Address object
+
+    """
     dt = normalize(date_time, 'datetime')
     restaurant_id = normalize(restaurant_id, 'number')
     subtotal = normalize(subtotal, 'money')
@@ -24,6 +50,12 @@ class RestaurantApi(OrdrinApi):
     return self._call_api('GET', ('fee', restaurant_id, subtotal, tip, dt, address.zip, address.city, address.addr))
 
   def get_details(self, restaurant_id):
-    """Call the rd API function with the given id."""
+    """Get details of the given restaurant, including contact information and
+    the menu
+
+    Arguments:
+    restaurant_id -- Ordr.in's restaurant identifier
+    
+    """
     restaurant_id = normalize(restaurant_id, 'number')
     return self._call_api('GET', ('rd', restaurant_id))
