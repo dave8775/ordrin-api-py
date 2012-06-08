42089e9c96756ecec9c2edf0e0ef163fab5f133e
Michael Lumish <michael.lumish@gmail.com>
2012-06-08 12:21:53 -0400

    Expanded user API documentation

--- ordrin/user.py
+++ ordrin/user.py
@@ -6,13 +6,24 @@ class UserApi(OrdrinApi):
   """This class will be used to access the user API"""
 
   def get(self, login):
-    """Gets account information for the user with the given email.
-    Hashes password with SHA256 before sending"""
+    """Gets account information for the user associated with login
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    
+    """
     return self._call_api('GET', ('u', login.email), login=login)
 
   def create(self, login, first_name, last_name):
-    """Creates account for the user with the given email. Throws a relevant exception
-    on failure."""
+    """Creates account for the user associated with login. Throws a relevant exception
+    on failure.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    first_name -- the user's first name
+    last_name -- the user's last name
+    
+    """
     data = {'email':login.email,
             'first_name':normalize(first_name, 'name'),
             'last_name':normalize(last_name, 'name'),
@@ -20,8 +31,15 @@ class UserApi(OrdrinApi):
     return self._call_api('POST', ('u', login.email), data=data)
 
   def update(self, login, first_name, last_name):
-    """Updates account for the user with the given email. Throws a relevant exception
-    on failure."""
+    """Updates account for the user associated with login. Throws a relevant exception
+    on failure.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    first_name -- the user's first name
+    last_name -- the user's last name
+    
+    """
     data = {'email':login.email,
             'first_name':normalize(first_name, 'name'),
             'last_name':normalize(last_name, 'name'),
@@ -29,34 +47,76 @@ class UserApi(OrdrinApi):
     return self._call_api('POST', ('u', login.email), login=login, data=data)
 
   def get_all_addresses(self, login):
-    """Get all addresses for the user with the given email."""
+    """Get a list of all saved addresses for the user associated with login.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    
+    """
     return self._call_api('GET', ('u', login.email, 'addrs'), login=login)
 
   def get_address(self, login, addr_nick):
-    """Get a particular address for the given user."""
+    """Get a saved address belonging to the logged in user by nickname.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    addr_nick -- the nickname of the address to get
+    
+    """
     return self._call_api('GET', ('u', login.email, 'addrs', normalize(addr_nick, 'nick')), login=login)
 
   def set_address(self, login, addr_nick, address):
-    """Set a particular address for the given user. Throws a relevant exception
-    on failure"""
+    """Save an address by nickname for the logged in user
+    Throws a relevant exception on failure
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    addr_nick -- the nickname of the address to save
+    address -- the address to save. Should be an ordrin.data.Address object
+    
+    """
     return self._call_api('PUT', ('u', login.email, 'addrs', normalize(addr_nick, 'nick')), login=login, data=address.make_dict())
       
   def remove_address(self, login, addr_nick):
-    """Delete a particular address for the given user. Throws a relevant exception
-    on failure"""
+    """Remove an address, saved by the logged in user, by nickname
+    Throws a relevant exception on failure.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    addr_nick -- the nickname of the address to remove
+    
+    """
     return self._call_api('DELETE', ('u', login.email, 'addrs', normalize(addr_nick, 'nick')), login=login)
 
   def get_all_credit_cards(self, login):
-    """Get all credit cards for the user with the given email."""
+    """Get a list of all saved credit cards for the user associated with login.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    
+    """
     return self._call_api('GET', ('u', login.email, 'ccs'), login=login)
 
   def get_credit_card(self, login, card_nick):
-    """Get a particular credit card for the given user."""
+    """Get a saved credit card belonging to the logged in user by nickname.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    card_nick -- the nickname of the credit card to get
+
+    """
     return self._call_api('GET', ('u', login.email, 'ccs', normalize(card_nick, 'nick')), login=login)
 
   def set_credit_card(self, login, card_nick, credit_card):
-    """Set a particular credit card for the given user. Throws a relevant exception
-    on failure"""
+    """Save an credit card by nickname for the logged in user
+    Throws a relevant exception on failure
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    card_nick -- the nickname of the credit card to save
+    credit_card -- the credit card to save. Should be an ordrin.data.CreditCard object
+    
+    """
     card_nick = normalize(card_nick, 'nick')
     data = credit_card.make_dict()
     data.update(login.make_dict())
@@ -65,20 +125,42 @@ class UserApi(OrdrinApi):
     return self._call_api('PUT', ('u', login.email, 'ccs', card_nick), login=login, data=data)
 
   def remove_credit_card(self, login, card_nick):
-    """Delete a particular credit card for the given user. Throws a relevant exception
-    on failure"""
+    """Remove an credit card, saved by the logged in user, by nickname
+    Throws a relevant exception on failure.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    card_nick -- the nickname of the credit card to remove
+    
+    """
     return self._call_api('DELETE', ('u', login.email, 'ccs', normalize(card_nick, 'nick')), login=login)
 
   def get_order_history(self, login):
-    """Get a list of previous orders."""
+    """Get a list of previous orders by the logged in user.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+
+    """
     return self._call_api('GET', ('u', login.email, 'orders'), login=login)
 
   def get_order_detail(self, login, order_id):
-    """Get details of a particular previous order."""
+    """Get details of a particular previous order by the logged in user.
+
+    Arguments:
+    login -- the user's login information. Should be an ordrin.data.UserLogin object
+    order_id -- The order ID
+
+    """
     return self._call_api('GET', ('u', login.email, 'orders', normalize(order_id, 'alphanum')), login=login)
 
   def set_password(self, login, new_password):
-    """Change the user's password."""
+    """Change the logged in user's password.
+
+    Arguments:
+    login -- the user's current login information. Should be an ordrin.data.UserLogin object
+    new_password -- the new password
+    """
     data = {'email': login.email,
             'password': UserLogin.hash_password(new_password),
             'previous_password': login.password}
